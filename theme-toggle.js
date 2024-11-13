3be146bd683867c723efd48990d4841d1e66c7bc
DaanixPL <dlitwin04@outlook.com>
2024-11-13 19:46:42 +0100

    Poprawino przycisk Mode

--- theme-toggle.js
+++ theme-toggle.js
@@ -1,28 +1,25 @@
-﻿// Funkcja do ustawiania motywu
+﻿// Funkcja do ustawiania motywu i tekstu przycisku
 function setTheme(themeName) {
-    console.log("Ustawianie motywu:", themeName);
-
     const mainElement = document.querySelector('main'); // Pobierz element <main>
+    const themeToggleButton = document.getElementById('theme-toggle'); // Pobierz przycisk
 
     // Dodanie klasy motywu do <body> i <main>
     if (themeName === 'light-theme') {
         document.body.classList.add('light-theme');
         document.body.classList.remove('dark-theme');
-
-        // Dodaj klasę light-theme do <main>
         if (mainElement) {
             mainElement.classList.add('light-theme');
             mainElement.classList.remove('dark-theme');
         }
+        themeToggleButton.textContent = "Dark Mode";
     } else {
         document.body.classList.add('dark-theme');
         document.body.classList.remove('light-theme');
-
-        // Dodaj klasę dark-theme do <main>
         if (mainElement) {
             mainElement.classList.add('dark-theme');
             mainElement.classList.remove('light-theme');
         }
+        themeToggleButton.textContent = "Light Mode";
     }
 
     // Zapisz motyw w localStorage
@@ -31,7 +28,6 @@ function setTheme(themeName) {
 
 // Funkcja do przełączania między motywami
 function toggleTheme() {
-    console.log("Przełączanie motywu...");
     if (document.body.classList.contains('light-theme')) {
         setTheme('dark-theme');
     } else {
